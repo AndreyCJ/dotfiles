7d52a364deac7b48e2a82c9f318a436c82e18376
Andrey Chebotar <andrey.chebotar@cashelec.ru>
2022-01-08 21:12:34 +0500

    fix: polybar multi monitor setup, screen-layouts positioning

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -9,6 +9,14 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 # Launch bar1 and bar2
 MONITORS=$(xrandr --query | grep " connected" | cut -d" " -f1)
 
-MONITORS=$MONITORS polybar example
+#MONITORS=$MONITORS polybar example
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
 
 echo "Bars launched..."
